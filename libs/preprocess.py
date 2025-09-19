238df98264cbc469c61e9b0790d7a8d90dccaf08
123jason689 <jason.melvin689@gmail.com>
2025-09-19 16:57:18 +0700

    Add model download script and update .gitignore; Add more to video processing functions to support Grounding DINO implementation

--- libs/preprocess.py
+++ libs/preprocess.py
@@ -1,14 +1,36 @@
-from typing import Iterator, List, overload, Union
+from typing import Iterator, List, overload, Union, Literal
+import groundingdino.datasets.transforms as T
+import torchvision.transforms.functional as F
+from groundingdino.util.misc import interpolate
+import torch
+import math
 import cv2
 import numpy as np
 
-def fps_scale_down(vid_file: cv2.VideoCapture, fps: int, as_itter: bool = False) -> Union[List[np.typing.ArrayLike], Iterator[np.typing.ArrayLike]]:
+def fps_scale_down_to_np_arr(vid_file: cv2.VideoCapture, fps: int, output_type: Literal['list', 'iter'] = 'list' ) -> Union[List[np.typing.ArrayLike], Iterator[np.typing.ArrayLike]]:
+    """
+    CAUTION
+    -------
+    This function is deprecated. Prefer using the to_tensor version of this function
+
+    Load and reduce the FPS by picking fragments the main video frames. 
+
+    Parameters
+    ----------
+    path : str
+        Path to the video file.
+
+    Returns
+    -------
+    np.ndarray
+        A NumPy array of shape (num_frames, H, W, 3), dtype=uint8,
+        containing all video frames in RGB order. Output in (B,H,W,C) format.
+    """
     if not vid_file.isOpened():
         raise ValueError("File is not opened, VideoCapture instance is empty. Make sure to load the video first")
 
     if fps == 0:
         ret, frame = vid_file.read()
-        vid_file.release()
         if not ret:
             return [] if not as_itter else iter(())
         arr = np.asarray(frame)
@@ -36,16 +58,16 @@ def fps_scale_down(vid_file: cv2.VideoCapture, fps: int, as_itter: bool = False)
             ret, frame = vid_file.read()
             if not ret:
                 break
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame_idx += 1
             timestamp_ms = vid_file.get(cv2.CAP_PROP_POS_MSEC) or (frame_idx / vid_fps * 1000.0 if vid_fps > 0 else frame_idx * interval_ms)
             if timestamp_ms + 1e-6 >= next_keep_ms:
                 next_keep_ms += interval_ms
-                yield np.asarray(frame)
+                yield np.asarray(frame_rgb)
             else:
                 dropped += 1
-        vid_file.release()
 
-    if as_itter:
+    if output_type == 'iter':
         return iterator()
 
     # collect into list
@@ -54,3 +76,213 @@ def fps_scale_down(vid_file: cv2.VideoCapture, fps: int, as_itter: bool = False)
 
     print(f"Shrink FPS from {vid_fps} FPS to {len(frames)} frames while dropping {dropped} frames")
     return frames
+
+def fps_scale_down_to_tensor(vid_file: cv2.VideoCapture, fps: int)->torch.Tensor:
+    if not vid_file.isOpened():
+        raise ValueError("File is not opened, VideoCapture instance is empty. Make sure to load the video first")
+
+    if fps == 0:
+        ret, frame = vid_file.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        tensor = torch.from_numpy(frame)
+        
+        return tensor
+
+    vid_fps = vid_file.get(cv2.CAP_PROP_FPS) or 0.0
+    if vid_fps > 0 and fps > vid_fps:
+        print("FPS larger than video fps. Defaulting to native video fps")
+        fps = int(vid_fps)
+
+    interval_ms = 1000.0 / float(fps)
+    next_keep_ms = 0.0
+    vid_frame_count = int(vid_file.get(cv2.CAP_PROP_FRAME_COUNT))
+    B = int(max(0, math.floor(((max(vid_frame_count - 1, 0) * float(fps)) / float(vid_fps))) + 1))
+    H = int(vid_file.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    W = int(vid_file.get(cv2.CAP_PROP_FRAME_WIDTH))
+    C = 3 # RGB
+    frames: torch.Tensor = torch.empty((B,H,W,C), dtype=torch.uint8)
+    dropped = 0
+
+    for frame_idx in range(B):
+        ret, frame = vid_file.read()
+        if not ret:
+            break
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        timestamp_ms = vid_file.get(cv2.CAP_PROP_POS_MSEC) or (frame_idx / vid_fps * 1000.0 if vid_fps > 0 else frame_idx * interval_ms)
+        if timestamp_ms + 1e-6 >= next_keep_ms:
+            next_keep_ms += interval_ms
+            frames[frame_idx] = torch.from_numpy(frame_rgb)
+        else:
+            dropped += 1
+
+    vid_file.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    print(f"Shrink FPS from {vid_fps} FPS to {len(frames)} frames while dropping {dropped} frames")
+    return frames
+
+
+def vid_to_np_arr(vid_file: cv2.VideoCapture) -> np.typing.NDArray[np.uint8]:
+    """
+    Load all frames from a video file.
+
+    Parameters
+    ----------
+    path : cv2.VideoCapture
+        A cv2.VideoCapture type object.
+
+    Returns
+    -------
+    np.ndarray
+        A NumPy array of shape (num_frames, H, W, 3), dtype=uint8,
+        containing all video frames in RGB order.
+    """
+
+    if not vid_file.isOpened():
+        raise ValueError("File is not opened, VideoCapture instance is empty. Make sure to load the video first")
+    
+    frames = []
+
+    while True:
+        ret, frame = vid_file.read()
+        if not ret:
+            break
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frames.append(frame_rgb)
+
+    return np.stack(frames)
+
+def vid_to_torch_tensor(vid_file: cv2.VideoCapture) -> torch.Tensor:
+    """
+    Load all frames from a video file to torch tensor format ready for .
+
+    Parameters
+    ----------
+    path : str
+        Path to the video file.
+
+    Returns
+    -------
+    np.ndarray
+        A NumPy array of shape (num_frames, H, W, 3), dtype=uint8,
+        containing all video frames in RGB order.
+    """
+    if not vid_file.isOpened():
+        raise ValueError("File is not opened, VideoCapture instance is empty. Make sure to load the video first")
+
+    B = int(vid_file.get(cv2.CAP_PROP_FRAME_COUNT))
+    H = int(vid_file.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    W = int(vid_file.get(cv2.CAP_PROP_FRAME_WIDTH))
+    C = 3 # RGB
+
+    frames: torch.Tensor = torch.empty((B,H,W,C), dtype=torch.uint8)
+
+    for frame_idx in range(B):
+        ret, frame = vid_file.read()
+        if not ret:
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frames[frame_idx] = torch.from_numpy(frame)
+
+    return frames
+
+def permute_BHWC_to_BCHW(img_tensor:torch.Tensor)->torch.Tensor:
+    return img_tensor.permute(0,3,1,2)
+
+## input (B,H,W,C)
+def load_frame_formated(frames:torch.Tensor):
+    if frames.shape[-1] < frames.shape[1]:
+        frames = permute_BHWC_to_BCHW(frames)
+    transform = T.Compose(
+        [
+            Resize_CV2(800, max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+    frames_transformed, _ = transform(frames, None)
+    return frames_transformed
+
+
+# helpers / altered from source code
+class Resize_CV2(object):
+    def __init__(self, size, max_size=None):
+        assert isinstance(size, (list, tuple))
+        self.size = size
+        self.max_size = max_size
+
+    def __call__(self, img, target=None):
+        return self.resize(img, target)
+    
+    
+    def resize(self, image, target):
+        '''
+        
+        
+        
+            This Implementation isn't finished, 
+            need to convert the process 
+            from accepting PIL.Image to torch.Tensor
+            with Tensor format (B,C,H,W)
+        
+        
+        
+        '''
+        # size can be min_size (scalar) or (w, h) tuple
+
+        def get_size_with_aspect_ratio(image_size, size, max_size=None):
+            w, h = image_size
+            if max_size is not None:
+                min_original_size = float(min((w, h)))
+                max_original_size = float(max((w, h)))
+                if max_original_size / min_original_size * size > max_size:
+                    size = int(round(max_size * min_original_size / max_original_size))
+
+            if (w <= h and w == size) or (h <= w and h == size):
+                return (h, w)
+
+            if w < h:
+                ow = size
+                oh = int(size * h / w)
+            else:
+                oh = size
+                ow = int(size * w / h)
+
+            return (oh, ow)
+
+        def get_size(image_size, size, max_size=None):
+            if isinstance(size, (list, tuple)):
+                return size[::-1]
+            else:
+                return get_size_with_aspect_ratio(image_size, size, max_size)
+
+        size = get_size(image.size, size, self.max_size)
+        rescaled_image = F.resize(image, size)
+
+        if target is None:
+            return rescaled_image, None
+
+        ratios = tuple(float(s) / float(s_orig) for s, s_orig in zip(rescaled_image.size, image.size))
+        ratio_width, ratio_height = ratios
+
+        target = target.copy()
+        if "boxes" in target:
+            boxes = target["boxes"]
+            scaled_boxes = boxes * torch.as_tensor(
+                [ratio_width, ratio_height, ratio_width, ratio_height]
+            )
+            target["boxes"] = scaled_boxes
+
+        if "area" in target:
+            area = target["area"]
+            scaled_area = area * (ratio_width * ratio_height)
+            target["area"] = scaled_area
+
+        h, w = size
+        target["size"] = torch.tensor([h, w])
+
+        if "masks" in target:
+            target["masks"] = (
+                interpolate(target["masks"][:, None].float(), size, mode="nearest")[:, 0] > 0.5
+            )
+
+        return rescaled_image, target
--- model_downloads.sh
+++ model_downloads.sh
@@ -1,13 +1,23 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-YOLO_PATH="$SCRIPT_DIR/models/yoloe/yolov8l_latest.pt"
+# YOLO_PATH="$SCRIPT_DIR/models/yoloe/yolov8l_latest.pt"
 
-if [ -f "$YOLO_PATH" ]; then
-    echo "Found data file at: $YOLO_PATH"
-else
-    echo "Downloading model weights into $YOLO_PATH"
-    curl -L https://github.com/ultralytics/assets/releases/download/v8.3.0/yoloe-v8l-seg.pt -o ${SCRIPT_DIR}/models/yoloe/yolov8l_latest.pt --create-dirs
-    echo "Model downloaded into $YOLO_PATH"
-fi
+# if [ -f "$YOLO_PATH" ]; then
+#     echo "Found data file at: $YOLO_PATH"
+# else
+#     echo "Downloading model weights into $YOLO_PATH"
+#     curl -L https://github.com/ultralytics/assets/releases/download/v8.3.0/yoloe-v8l-seg.pt -o ${SCRIPT_DIR}/models/yoloe/yolov8l_latest.pt --create-dirs
+#     echo "Model downloaded into $YOLO_PATH"
+# fi
+
+export CUDA_HOME="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.3"
+echo $CUDA_HOME
+git clone https://github.com/IDEA-Research/GroundingDINO.git "$SCRIPT_DIR/models/dino/GroundingDINO/"
+cd $SCRIPT_DIR/models/dino/GroundingDINO/
+pip install -e .
+mkdir weights
+cd weights
+wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
+cd $SCRIPT_DIR
 
 
