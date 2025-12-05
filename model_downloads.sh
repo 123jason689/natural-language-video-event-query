92337e654e2933b516dfc6c3f6a8f793d204bbff
123jason689 <145532519+123jason689@users.noreply.github.com>
2025-12-05 13:21:39 +0700

    Enhance model_downloads.sh for CUDA compatibility
    Update model_downloads.sh to modify CUDA files and install requirements.

--- model_downloads.sh
+++ model_downloads.sh
@@ -13,8 +13,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export CUDA_HOME="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.3"
 echo $CUDA_HOME
 git clone https://github.com/IDEA-Research/GroundingDINO.git "$SCRIPT_DIR/models/dino/GroundingDINO/"
+cd $SCRIPT_DIR/models/dino/GroundingDINO/groundingdino/models/GroundingDINO/csrc/MsDeformAttn/
+sed -i 's/value.type()/value.scalar_type()/g' ms_deform_attn_cuda.cu
+sed -i 's/value.scalar_type().is_cuda()/value.is_cuda()/g' ms_deform_attn_cuda.cu
+sed -i 's/value.type()/value.scalar_type()/g' ms_deform_attn_cpu.cpp
+sed -i 's/value.type().is_cuda()/value.is_cuda()/g' ms_deform_attn.h
+grep "value.scalar_type()" ms_deform_attn_cuda.cu | head -n 2
 cd $SCRIPT_DIR/models/dino/GroundingDINO/
-pip install -e .
+cp $SCRIPT_DIR/libs/setup_edited_default_scripts/gdino_requirements.txt ./requirements.txt
+pip install -v --no-build-isolation -e .
 mkdir weights
 cd weights
 curl -s -L -O https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
