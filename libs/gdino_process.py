28db9e00d5f33984745aefa7490bc5f14975f7ed
123jason689 <jason.melvin689@gmail.com>
2025-12-14 13:58:21 +0700

    I'm unbelievably stupid

--- libs/gdino_process.py
+++ libs/gdino_process.py
@@ -93,7 +93,6 @@ class Model:
                     timestamp_ms=float(frame_batch.timestamps_ms[i].item()),
                     detections=detections,
                     phrases=phrases,
-                    ## boxes_cxcywh
                     boxes_cxcywh=boxes.cpu(),
                     logits=logits.cpu(),
                 )
@@ -169,8 +168,8 @@ class Model:
                     timestamp_ms=float(frame_batch.timestamps_ms[i].item()),
                     detections=detections,
                     phrases=phrases,
-                    cxcyhw = boxes.cpu(),
-                    logits = logits.cpu()
+                    boxes_cxcywh= boxes.cpu(),
+                    logits= logits.cpu()
                 )
             )
 
